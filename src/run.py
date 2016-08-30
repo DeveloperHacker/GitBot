241eb9e5e18f507579f8c14b8f428f6a013e6ac4
MadCat <seryoga@computercraft.ru>
2016-08-30 16:25:09 +0300

    Rename: NewGitHandler to GitHandler

--- src/run.py
+++ src/run.py
@@ -1,5 +1,4 @@
-# from src.main.GitHandler import GitHandler
-from src.main.NewGitHandler import GitHandler
+from src.main.GitHandler import GitHandler
 
 handler = GitHandler(bot_nick="PythonBot")
 handler.start()
